3e34ef8e616ff83ffb20212b33431e224fb701ad
Sandagomi <sandagomitk@gmail.com>
2020-03-31 20:39:17 +0530

    second commit

--- src/Inheritance/Find.jsx
+++ src/Inheritance/Find.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import "./Find.css";
 
 class Text extends Component {
   constructor(props) {
@@ -36,26 +37,134 @@ class Text extends Component {
   render() {
     return (
       <div>
-        <input type="file" onChange={this.showFile} />
         <div
-          id="show-text"
           style={{
             border: "solid",
-            width: "600px",
-            height: "400px",
-            marginLeft: "450px",
-            marginTop: "20px"
+            width: "1520px",
+            alignContent: "center",
+            marginLeft: "5px",
+            marginTop: "5px",
+            backgroundColor: "#2d545e",
+            color: "#feb300"
           }}
         >
-          Choose text File
+          <h1>CALCULATE INHERITANCE</h1>
         </div>
 
-        <button id="countInh" onClick={this.textCount}>
-          Count the direct INH
-        </button>
+        <input
+          type="file"
+          style={{
+            marginTop: "20px",
+            marginLeft: "-1000PX"
+          }}
+          onChange={this.showFile}
+        />
+        <br></br>
+        <div
+          style={{
+            marginTop: "10px"
+          }}
+        >
+          <div
+            id="show-text"
+            style={{
+              border: "solid #feb300",
+              width: "600px",
+              height: "400px",
+              marginLeft: "100px",
+              margintop: "30px",
+              paddingTop: "10px",
+              backgroundColor: "#FFFFFF"
+            }}
+          >
+            Choose text File
+          </div>
+        </div>
+        <br></br>
+        <div
+          style={{
+            marginTop: "-15px"
+          }}
+        >
+          <button
+            style={{
+              paddingBlock: "10px",
+              marginLeft: "-1100px",
+              textAlign: "center",
+              backgroundColor: "#2d545e",
+              color: "#feb300"
+            }}
+            id="countInh"
+            onClick={this.textCount}
+          >
+            Count the direct INH
+          </button>
+        </div>
+
         <br />
-        <div className="mark">
-          Number of Inheritances counted in the code: <span id="numb"></span>
+        {/* <div className="mark">
+          Number of Inheritances counted in the code: <span id=""></span>
+        </div> */}
+
+        <div
+          className="inhTable"
+          style={{
+            marginLeft: "770px",
+            marginTop: "-500px",
+            border: "solid #c7af6b",
+            borderBlockEndWidth: "10px",
+            borderBlockWidth: "20px",
+            width: "700px",
+            height: "400px",
+            backgroundColor: "#2d545e"
+          }}
+        >
+          <table className="table">
+            <tr>
+              <th>Count</th>
+              <th>Class Name </th>
+              <th>No. Direct Inheritance</th>
+              <th>No. Indirect Inheritance</th>
+              <th>No. Total Inheritance</th>
+              <th>CI</th>
+            </tr>
+            <tr>1</tr>
+            <tr>2</tr>
+            <tr>3</tr>
+            <tr>4</tr>
+            <tr>5</tr>
+            <tr>6</tr>
+            <tr>7</tr>
+            <tr>8</tr>
+            <tr></tr>
+            <tr></tr>
+            <tr></tr>
+
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td>
+                <div className="mark">
+                  Number of Total Inheritances :<span id="numb">[ ]</span>
+                </div>
+              </td>
+            </tr>
+          </table>
+        </div>
+        <div
+          style={{
+            border: "solid",
+            width: "1520px",
+            alignContent: "center",
+            marginLeft: "5px",
+            marginTop: "90px",
+            backgroundColor: "#2d545e",
+            color: "#feb300"
+          }}
+        >
+          <h1>Project by SLIIT.</h1>
         </div>
       </div>
     );
